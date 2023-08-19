4254cefd7e2472cfc7039995d47cabe3c562bfd0
juanvelasco1 <juanesvb1@gmail.com>
2023-08-18 19:13:53 -0500

    2

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ function computeGrades(grades) {
         C: [70, 79],
         D: [60, 69],
         F: [0, 59]
+        
     };
+    
     const limitsNotes = [];
     let parsed = [A,F,D,C,B,A,F];
     let passed = [5];
     let lose = [2];
     let average = [73];
+    
 
-    console.log("hola")
+    
 }
-
+console.log(computeGrades)
+console.log(limitsNotes)
 computeGrades();
 limitsNotes();
 
